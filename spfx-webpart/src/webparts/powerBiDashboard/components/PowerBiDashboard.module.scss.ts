54d7e7969e28ebe3f46bc1beef91c8984f1c005b
Derek Gallardo <derekgallardo01@gmail.com>
2026-03-30 19:47:20 -0400

    Fix build: add webpack-env types, update lib to es2016

---
+++ spfx-webpart/src/webparts/powerBiDashboard/components/PowerBiDashboard.module.scss.ts
@@ -0,0 +1,28 @@
+
+require("./PowerBiDashboard.module.css");
+const styles = {
+  powerBiDashboard: 'powerBiDashboard_3cfb7c20',
+  statusBar: 'statusBar_3cfb7c20',
+  statusDot: 'statusDot_3cfb7c20',
+  statusConnected: 'statusConnected_3cfb7c20',
+  statusConnecting: 'statusConnecting_3cfb7c20',
+  pulse: 'pulse_3cfb7c20',
+  statusError: 'statusError_3cfb7c20',
+  statusDisconnected: 'statusDisconnected_3cfb7c20',
+  statusText: 'statusText_3cfb7c20',
+  lastRefreshed: 'lastRefreshed_3cfb7c20',
+  refreshButton: 'refreshButton_3cfb7c20',
+  errorBanner: 'errorBanner_3cfb7c20',
+  dismissButton: 'dismissButton_3cfb7c20',
+  loadingOverlay: 'loadingOverlay_3cfb7c20',
+  spinner: 'spinner_3cfb7c20',
+  spin: 'spin_3cfb7c20',
+  embedContainer: 'embedContainer_3cfb7c20',
+  placeholder: 'placeholder_3cfb7c20',
+  placeholderIcon: 'placeholderIcon_3cfb7c20',
+  placeholderTitle: 'placeholderTitle_3cfb7c20',
+  placeholderDescription: 'placeholderDescription_3cfb7c20',
+  configureButton: 'configureButton_3cfb7c20'
+};
+
+export default styles;
